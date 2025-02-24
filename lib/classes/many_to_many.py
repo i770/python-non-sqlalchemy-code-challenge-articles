163e2d1d4c7d6f2223b66c7e085e6b56eae209cc
i770 <diegomudaida@gmail.com>
2025-02-25 01:23:18 +0300

    initial commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,24 +1,30 @@
 class Article:
+    instances = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
+        if len(title) < 5 or len(title) > 50:
+            raise ValueError("Title must be between 5 and 50 characters.")
         self.title = title
-        
+        Article.instances.append(self)
+
 class Author:
     def __init__(self, name):
         self.name = name
 
     def articles(self):
-        pass
+        return [article for article in Article.instances if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set(article.magazine for article in Article.instances if article.author == self))
 
     def add_article(self, magazine, title):
-        pass
+        new_article = Article(self, magazine, title)
+        return new_article
 
     def topic_areas(self):
-        pass
+        return sorted(set(magazine.category for magazine in self.magazines()))
 
 class Magazine:
     def __init__(self, name, category):
@@ -26,13 +32,27 @@ class Magazine:
         self.category = category
 
     def articles(self):
-        pass
+        return [article for article in Article.instances if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in Article.instances if article.magazine == self))
 
     def article_titles(self):
-        pass
+        return [article.title for article in Article.instances if article.magazine == self]
 
     def contributing_authors(self):
-        pass
+        author_counts = {}
+        for article in Article.instances:
+            if article.magazine == self:
+                author_counts[article.author] = author_counts.get(article.author, 0) + 1
+        return [author for author, count in author_counts.items() if count > 0]
+
+
+    @classmethod
+    def top_publisher(cls):
+        magazine_counts = {}
+        for article in Article.instances:
+            magazine_counts[article.magazine] = magazine_counts.get(article.magazine, 0) + 1
+        if not magazine_counts:
+            return None
+        return max(magazine_counts, key=magazine_counts.get)
